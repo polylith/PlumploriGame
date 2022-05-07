975ad955d6f014e2fed63092749b55be92e8f9d8
Daniel <78366151+dguenther677@users.noreply.github.com>
2022-05-07 13:17:21 +0200

    Update CursorInactive.cs

--- Assets/Scripts/UI/CursorInactive.cs
+++ Assets/Scripts/UI/CursorInactive.cs
@@ -10,22 +10,9 @@ public class CursorInactive : MonoBehaviour, IPointerEnterHandler
 {
     public void OnPointerEnter(PointerEventData eventData)
     {
-        ActionController actionController = ActionController.GetInstance();
-        
-        if (eventData.pointerEnter != gameObject
-            ||(actionController.IsCurrentAction(typeof(UseAction))
-            || actionController.IsCurrentAction(typeof(GrabAction)))
-            && actionController.IsCurrentActionActive())
+        if (eventData.pointerEnter != gameObject)
             return;
 
-        actionController.UnsetActionState(null);
-        
-        UIDropPoint uiDropPoint = UIDropPoint.GetInstance();
-        uiDropPoint.IsFreezed = false;
-        uiDropPoint.HidePointer();
-        
-        GameManager.GetInstance().UnHighlight();
-
-        UIGame.GetInstance().SetCursorEnabled(false, false);
+        ActionController.GetInstance().HandleCursor();
     }
 }
